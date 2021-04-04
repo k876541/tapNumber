b661e4fe7836ea67cd35c48e8dd451cc7077be54
ryan_chang <k876541@hotmail.com>
2021-04-04 23:52:35 +0800

    Initial Commit

---
+++ tapNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tapNumber
+//
+//  Created by Ryan Chang on 2021/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
